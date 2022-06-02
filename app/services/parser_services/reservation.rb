0052c836fad1b82c7ef2565bc0cc45acda4bb37a
rgabriel01 <raymond.m.gabriel@gmail.com>
2022-06-02 20:41:22 +0800

    ADD: reservation parser handler for 2nd format

--- app/services/parser_services/reservation.rb
+++ app/services/parser_services/reservation.rb
@@ -29,61 +29,73 @@ module ParserServices
 
     def parse_code
       return @payload[:reservation_code] if @payload.key?('reservation_code')
+      return @payload[:reservation][:code] if @payload.key?('reservation')
       ''
     end
 
     def parse_start_date
       return @payload[:start_date] if @payload.key?('start_date')
+      return @payload[:reservation][:start_date] if @payload.key?('reservation')
       ''
     end
 
     def parse_end_date
       return @payload[:end_date] if @payload.key?('end_date')
+      return @payload[:reservation][:end_date] if @payload.key?('reservation')
       ''
     end
 
     def parse_nights
       return @payload[:nights] if @payload.key?('nights')
+      return @payload[:reservation][:nights] if @payload.key?('reservation')
       0
     end
 
     def parse_adult_guest_count
       return @payload[:adults] if @payload.key?('adults')
+      return @payload[:reservation][:guest_details][:number_of_adults] if @payload.key?('reservation')
       0
     end
 
     def parse_children_guest_count
       return @payload[:children] if @payload.key?('children')
+      return @payload[:reservation][:guest_details][:number_of_children] if @payload.key?('reservation')
       0
     end
 
     def parse_infant_guest_count
       return @payload[:infants] if @payload.key?('infants')
+      return @payload[:reservation][:guest_details][:number_of_infants] if @payload.key?('reservation')
       0
     end
 
     def parse_security_price
       return @payload[:security_price] if @payload.key?('security_price')
+      return @payload[:reservation][:listing_security_price_accurate] if @payload.key?('reservation')
       0
     end
 
     def parse_payout_price
       return @payload[:payout_price] if @payload.key?('payout_price')
+      return @payload[:reservation][:expected_payout_amount] if @payload.key?('reservation')
       0
     end
 
     def parse_total_price
       return @payload[:total_price] if @payload.key?('total_price')
+      return @payload[:reservation][:total_paid_amount_accurate] if @payload.key?('reservation')
       0
     end
 
     def parse_currency
       return @payload[:currency] if @payload.key?('currency')
+      return @payload[:reservation][:host_currency] if @payload.key?('reservation')
       ''
     end
 
     def parse_status
       return @payload[:status] if @payload.key?('status')
+      return @payload[:reservation][:status_type] if @payload.key?('reservation')
       ''
     end
   end
